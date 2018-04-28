6bb644191f72f2114e186e528bd959b7da8a4f5c
Milos777 <milos.bjelcevic@diwanee.com>
2018-04-28 20:03:11 +0200

    update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var WM = (function($) {
       button.on('click', function(){
         var offset = 20;
         $('html, body').animate({
-            scrollTop: $(".products").offset().top + offset
+            scrollTop: $(".scroll").offset().top + offset
         }, 500);
       });
       window.wasScrolled = false;
@@ -28,10 +28,75 @@ var WM = (function($) {
   };
   var slider = function(){
     $('.products').slick();
+    $('.about-us-second').slick();
+  }
+  var changeLogo = function(){
+    if($('body').hasClass('product-page')){
+        $('.navigation').find('div:nth-child(2)').find('img').attr("src","img/black-logo.png");
+        $('.navigation').find('div:last-child').find('img').attr("src","img/black-cart.png");
+      }
+  }
+
+  var modal = function(){
+      // Get the modal
+      var modal = document.getElementById('myModal');
+
+      // Get the button that opens the modal
+      var btn = document.getElementById("myBtn");
+
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks on the button, open the modal
+      btn.onclick = function() {
+          modal.style.display = "block";
+      }
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+          modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+          if (event.target == modal) {
+              modal.style.display = "none";
+          }
+      }
+  }
+  var subscribe = function(){
+    // Get the modal
+    var modal = document.getElementById('myModalSecond');
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtnSecond");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("closeSecond")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
   }
   var ready = function() {
     scrollClick();
     slider();
+    changeLogo();
+    modal();
+    subscribe();
   };
 
   // Only expose the ready function to the world
